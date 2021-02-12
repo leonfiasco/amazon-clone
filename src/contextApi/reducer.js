e859f683a3674c8c5eecdd942852eb49dad4c9b4
leon Boakye-Adjei <leon_adjei@hotmail.com>
2021-02-12 17:33:50 +0000

    adding basket functionality

--- src/contextApi/reducer.js
+++ src/contextApi/reducer.js
@@ -18,6 +18,26 @@ const reducer = (state, action) => {
                 //on the basket such as adding or removing item
                 basket: [...state.basket, action.item],
             };
+        
+        case "REMOVE_FROM_BASKET":
+            // finds first instance of value
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
+            // if the index is greater than 0 then that mean it has found an item
+            if (index >= 0) {
+                newBasket.splice(index, 1)
+            } else {
+                console.warn(
+                `Cant remove product (id: ${action.id}) as its not in basket`
+                );
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
 
         default: 
             return state;
