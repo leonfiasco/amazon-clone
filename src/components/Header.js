cd6b1864e33a7a78b5f9db5e92a6626e6093308d
leon Boakye-Adjei <leon_adjei@hotmail.com>
2021-02-21 20:56:36 +0000

    added user name to sign-in/sign-up

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,6 +17,7 @@ function Header() {
         }
     }
 
+
     return (
         <div className="header">
             <Link to="/">
@@ -27,28 +28,28 @@ function Header() {
                 <SearchIcon className="header_searchIcon"/>
             </div>    
             <div className="header_nav">
-                <Link to="/login">
+            {/* stops the page redirecting to login form when a user signs out 
+            if user not found then go to login page */}
+                <Link to={!user && "/login"}>
                     <div className="header_option" onClick={handleAuthentication}>
-                        <span className="header_optionLineOne">Hello Guest</span>
+                        <span className="header_optionLineOne">{user ? `Hello ${user.email.substring(0, user.email.indexOf('@'))}` : "Hello Guest"}</span>
                         <span className="header_optionLineTwo">{user ? "Sign Out" : "Sign In"}</span>
                     </div>
                 </Link>
-                    <div className="header_option">
-                        <span className="header_optionLineOne">Returns</span>
-                        <span className="header_optionLineTwo">& Orders</span>
-                    </div>
-
-                    <div className="header_option">
-                        <span className="header_optionLineOne">Your</span>
-                        <span className="header_optionLineTwo">Prime</span>
-                    </div>
-                    <Link to="/checkout">
+                <div className="header_option">
+                    <span className="header_optionLineOne">Returns</span>
+                    <span className="header_optionLineTwo">& Orders</span>
+                </div>
+                <div className="header_option">
+                    <span className="header_optionLineOne">Your</span>
+                    <span className="header_optionLineTwo">Prime</span>
+                </div>
+                <Link to="/checkout">
                     <div className="header_optionBasket">
                         <ShoppingBasketIcon />
                         <span className="header_optionLineTwo header_basketCount">{basket?.length}</span>
                     </div>
-                    </Link>
-
+                </Link>
             </div>
             
         </div>
