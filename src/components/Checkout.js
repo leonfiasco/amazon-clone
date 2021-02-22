e5509a065308e78d5ea25a88357cb7f7c852355d
leon Boakye-Adjei <leon_adjei@hotmail.com>
2021-02-22 12:54:27 +0000

    added firebase deployment

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -6,12 +6,13 @@ import { useStateValue } from "../contextApi/StateProvider";
 import "../styles/Checkout.css";
 
 function Checkout() {
-    const [{ basket }] = useStateValue();
+    const [{ basket, user }] = useStateValue();
     return (
         <div className="checkout">
             <div className="checkout_left">
                 <img className="checkout_ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
                 <div>
+                    <h3>{user ? `Hello, ${user.email.substring(0, user.email.indexOf('@'))}` : ''}</h3>
                     <h2 className="checkout_title">Your shopping Basket</h2>
                     {basket.map(item => (
                         <CheckoutProduct 
