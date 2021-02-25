4e6c12f445e277a1b12aebf576c4bdbcc91ca4a1
leon Boakye-Adjei <leon_adjei@hotmail.com>
2021-02-25 13:04:08 +0000

    styling payment page

--- src/components/Payement.js
+++ src/components/Payement.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from '../contextApi/StateProvider';
+import { Link } from 'react-router-dom';
 
 import '../styles/Payment.css';
 
@@ -9,6 +11,9 @@ function Payement() {
 	return (
 		<div className='payment'>
 			<div className='payment_container'>
+				<h1>
+					Checkout (<Link to='/checkout'>{basket?.length} items</Link>)
+				</h1>
 				{/* Payment section - delivery address */}
 				<div className='payment_section'>
 					<div className='payment_title'>
@@ -16,12 +21,34 @@ function Payement() {
 					</div>
 					<div className='payment_address'>
 						<p>{user?.email}</p>
+						<p>130 Leonard Drive</p>
+						<p>London, Uk</p>
 					</div>
 				</div>
 				{/* Payment section - Review Items */}
-				<div className='payment_section'></div>
+				<div className='payment_section'>
+					<div className='payment_tile'>
+						<h3>Review items and delivery</h3>
+					</div>
+					<div className='payment_items'>
+						{basket.map((item) => (
+							<CheckoutProduct
+								id={item.id}
+								title={item.title}
+								image={item.image}
+								price={item.price}
+								rating={item.rating}
+							/>
+						))}
+					</div>
+				</div>
 				{/* Payment section - payment method */}
-				<div className='payment_section'></div>
+				<div className='payment_section'>
+					<div className='payment_tile'>
+						<h3>Payment</h3>
+					</div>
+					<div className='payment_details'></div>
+				</div>
 			</div>
 		</div>
 	);
