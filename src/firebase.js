91636fb4811e578aab13b2fbea82f11378f1b612
leon Boakye-Adjei <leon_adjei@hotmail.com>
2021-02-28 05:26:21 +0000

    protecting keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,20 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD9c0C1ESrFAV00NO_nCezRNpsiIzx_D3g",
-    authDomain: "clone-771d6.firebaseapp.com",
-    projectId: "clone-771d6",
-    storageBucket: "clone-771d6.appspot.com",
-    messagingSenderId: "313867375818",
-    appId: "1:313867375818:web:4cc3ec318b3edd85095033",
-    measurementId: "G-ELK3E16H60"
-  };
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: 'clone-771d6.firebaseapp.com',
+	projectId: 'clone-771d6',
+	storageBucket: 'clone-771d6.appspot.com',
+	messagingSenderId: '313867375818',
+	appId: '1:313867375818:web:4cc3ec318b3edd85095033',
+	measurementId: 'G-ELK3E16H60',
+};
 
-  // initialising firebase
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
+// initialising firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-  // initialising database
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
+// initialising database
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
 
-  export { db, auth };
-
+export { db, auth };
